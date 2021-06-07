81051700105c45d3d38526dc50402071f5471632
nata2456 <56667840+nata2456@users.noreply.github.com>
2021-06-07 16:09:44 +0300

    Update Chain.cs

--- cs/Chain.cs
+++ cs/Chain.cs
@@ -4,6 +4,81 @@ using System.Text.Json;
 
 namespace ChainOfResponsibility
 {
+    
+    class Request
+    {
+        public String Login { get; set; }
+        public String Password { get; set; }
+
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+
+    class NotRobot : AbstractHandler
+    {
+        public override object Handle(object request)
+        {
+            Random rnd = new Random();
+            int a = rnd.Next(1, 100);
+            int b = rnd.Next(1, 100);
+            Console.WriteLine($"{a} + {b} = ");
+            int sum = a + b;
+            int counter = 3;
+            for (int i = 0; i < counter; i++)
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                if (input == sum)
+                {
+                    return base.Handle(request);
+                }
+                else
+                {
+                    Console.WriteLine("Wrong number.");
+
+                }
+            }
+            return null;
+
+        }
+    }
+
+    class AuthorizeHandler : AbstractHandler
+    {
+        private bool Check(string Login, string Password)
+        {
+            return Login == "user" && Password == "user";
+        }
+        public override object Handle(object request)
+        {
+            Console.WriteLine("Authorize");
+
+            int counter = 3;
+            for (int i = 0; i < counter; i++)
+            {
+                Console.WriteLine("Enter login ");
+                string login = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("Enter password ");
+                string password = Convert.ToString(Console.ReadLine());
+                Request req = new Request();
+                req.Login = login;
+                req.Password = password;
+                if (Check(req.Login, req.Password))
+                {
+                    return base.Handle(request);
+                }
+                else
+                {
+                    Console.WriteLine("Wrong login or password");
+
+                }
+            }
+            return null;
+
+        }
+    }
 
     
     class SmsHandler : AbstractHandler
